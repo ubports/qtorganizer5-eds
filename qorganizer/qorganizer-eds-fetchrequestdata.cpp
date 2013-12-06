284c07958ac3a710d2bcddb4b9f22f7fd8fd7185
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2013-12-06 13:45:36 -0300

    Fixed default value for endDate.

--- qorganizer/qorganizer-eds-fetchrequestdata.cpp
+++ qorganizer/qorganizer-eds-fetchrequestdata.cpp
@@ -67,7 +67,7 @@ time_t FetchRequestData::endDate() const
     if (endDate.isValid()) {
         return endDate.toTime_t();
     } else {
-        return LONG_MAX;
+        return QDateTime::fromMSecsSinceEpoch(std::numeric_limits < qint64 >::max()).toTime_t();
     }
 }
 
